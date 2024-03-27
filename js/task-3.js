bd465aebc9515a4ec122df7eba93a5640780aa41
Yana <yanassam81@gmail.com>
2024-03-27 13:25:14 +0100

    fix: task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
-const input = document.getElementById("name-input");
-const output = document.getElementById("name-output");
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
 function changeName() {
   const withoutSpace = input.value.trim();
